adfe35c6ae32eb90c60f14619e7e4c8a72d8a9d3
riffon3000 <riffon@gmail.com>
2018-11-27 21:16:22 -0600

    index.handlebars

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var app = express();
-var PORT = process.env.PORT || 5000;
-var exphbs = require('express-handlebars');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const exphbs = require('express-handlebars');
 
 app.use(express.static(path.join(__dirname, './public')));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var router = require('./controllers/burgers_controller.js');
+const router = require('./controllers/controller.js');
 app.use('/', router);
 
 // Server is listening on...
